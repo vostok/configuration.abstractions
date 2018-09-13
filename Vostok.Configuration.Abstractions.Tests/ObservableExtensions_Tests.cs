52f9bf0e05f174508ee300c27620cfc98f55593a
Krait <krait@skbkontur.ru>
2018-09-13 18:18:00 +0500

    Added observable extensions.

---
+++ Vostok.Configuration.Abstractions.Tests/ObservableExtensions_Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NSubstitute;
+using NUnit.Framework;
+using Vostok.Configuration.Abstractions.Extensions;
+
+namespace Vostok.Configuration.Abstractions.Tests
+{
+    [TestFixture]
+    internal class ObservableExtensions_Tests
+    {
+        private IObservable<int> numbers;
+        private IObserver<int> observer;
+        private List<int> received;
+
+        [SetUp]
+        public void TestSetup()
+        {
+            received = new List<int>();
+
+            numbers = Substitute.For<IObservable<int>>();
+            numbers.Subscribe(Arg.Do<IObserver<int>>(o => observer = o));
+        }
+
+        [Test]
+        public void Subscribe_with_OnNext_should_call_provided_action_on_next_item()
+        {
+            numbers.Subscribe(i => received.Add(i));
+
+            observer.OnNext(1);
+            observer.OnNext(2);
+
+            received.Should().Equal(1, 2);
+        }
+
+
+        [Test]
+        public void Subscribe_with_OnNext_should_throw_on_error()
+        {
+            numbers.Subscribe(i => { });
+
+            new Action(() => observer.OnError(new Exception("test"))).Should().Throw<Exception>();
+        }
+
+        [Test]
+        public void Subscribe_with_OnNext_and_OnError_should_call_provided_action_on_next_item()
+        {
+            numbers.Subscribe(i => received.Add(i), e => { });
+
+            observer.OnNext(1);
+            observer.OnNext(2);
+
+            received.Should().Equal(1, 2);
+        }
+
+        [Test]
+        public void Subscribe_with_OnNext_and_OnError_should_call_provided_action_on_error()
+        {
+            Exception error = null;
+            numbers.Subscribe(i => { }, e => error = e);
+
+            var expectedError = new Exception();
+            observer.OnError(expectedError);
+
+            error.Should().BeSameAs(expectedError);
+        }
+    }
+}
---
+++ Vostok.Configuration.Abstractions/Extensions/ObservableExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Runtime.ExceptionServices;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Abstractions.Extensions
+{
+    /// <summary>
+    /// Provides a set of static methods for subscribing delegates to observables.
+    /// </summary>
+    [PublicAPI]
+    public static class ObservableExtensions
+    {
+        /// <summary>
+        /// Subscribes an element handler to an observable sequence.
+        /// </summary>
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (onNext == null)
+                throw new ArgumentNullException(nameof(onNext));
+
+            return source.Subscribe(new AnonymousObserver<T>(onNext));
+        }
+
+        /// <summary>
+        /// Subscribes an element handler and an exception handler to an observable sequence.
+        /// </summary>
+        public static IDisposable Subscribe<T>(this IObservable<T> source, Action<T> onNext, Action<Exception> onError)
+        {
+            if (source == null)
+                throw new ArgumentNullException(nameof(source));
+            if (onNext == null)
+                throw new ArgumentNullException(nameof(onNext));
+            if (onError == null)
+                throw new ArgumentNullException(nameof(onError));
+
+            return source.Subscribe(new AnonymousObserver<T>(onNext, onError));
+        }
+
+        private class AnonymousObserver<T> : IObserver<T>
+        {
+            private readonly Action<T> onNext;
+            private readonly Action<Exception> onError;
+
+            public AnonymousObserver(Action<T> onNext, Action<Exception> onError = null)
+            {
+                this.onNext = onNext;
+                this.onError = onError;
+            }
+
+            public void OnCompleted()
+            {
+            }
+
+            public void OnError(Exception error)
+            {
+                if (onError == null)
+                    ExceptionDispatchInfo.Capture(error).Throw();
+                else
+                    onError(error);
+            }
+
+            public void OnNext(T value) => onNext(value);
+        }
+    }
+}

b64de91890d7ef22a285dc3a58ea1b03b8eaf42e
iloktionov <iloktionov@skbkontur.ru>
2019-02-18 12:33:54 +0500

    + SecretAttribute.

---
+++ Vostok.Configuration.Abstractions/Attributes/SecretAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Abstractions.Attributes
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// <para>Marks a field or property secret. External loggers should not expose values of settings marked with this attribute.</para>
+    /// <para>Useful for sensitive settings, such as API-keys, passwords and tokens.</para>
+    /// </summary>
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class SecretAttribute : Attribute
+    {
+    }
+}

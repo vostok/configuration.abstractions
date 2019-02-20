6be4d5cad94936c360a293ae4545783268fa1c4d
Krait <krait@skbkontur.ru>
2019-02-21 01:12:49 +0500

    Added [BindBy] attribute.

---
+++ Vostok.Configuration.Abstractions/Attributes/BindByAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Abstractions.Attributes
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Specifies that settings of this type should be bound by a settings binder of provided type.
+    /// </summary>
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class BindByAttribute : Attribute
+    {
+        public BindByAttribute([NotNull] Type binderType) => 
+            BinderType = binderType ?? throw new ArgumentNullException(nameof(binderType));
+
+        /// <summary>
+        /// The specified binder type.
+        /// </summary>
+        [NotNull]
+        public Type BinderType { get; }
+    }
+}

0f13d1f6f5d0b4eb12f92e547cf12c86a9af0801
Krait <krait@skbkontur.ru>
2019-02-21 01:08:07 +0500

    Moved ISettingsBinder<T> to vostok.configuration.

--- Vostok.Configuration.Abstractions/ISettingsBinder.cs
+++ Vostok.Configuration.Abstractions/ISettingsBinder.cs
@@ -4,20 +4,6 @@ using Vostok.Configuration.Abstractions.SettingsTree;
 namespace Vostok.Configuration.Abstractions
 {
     /// <summary>
-    /// Implements binding of <see cref="ISettingsNode"/>s to specific models.
-    /// </summary>
-    [PublicAPI]
-    public interface ISettingsBinder<out TSettings>
-    {
-        /// <summary>
-        /// <para>Binds the provided <see cref="ISettingsNode"/> instance to type <typeparamref name="TSettings"/>.</para>
-        /// <para>An exception will be thrown if the binding fails.</para>
-        /// </summary>
-        [CanBeNull]
-        TSettings Bind([CanBeNull] ISettingsNode rawSettings);
-    }
-
-    /// <summary>
     /// Implements binding of <see cref="ISettingsNode"/> to specific models.
     /// </summary>
     [PublicAPI]

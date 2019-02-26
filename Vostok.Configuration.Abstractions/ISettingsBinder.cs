135d2c970765b648a7a020188550ad46b9098028
iloktionov <iloktionov@skbkontur.ru>
2019-02-26 16:32:43 +0500

    Brought back generic ISettingsBinder interface.

---
+++ Vostok.Configuration.Abstractions/ISettingsBinder.cs
@@ -0,0 +1,19 @@
+﻿using JetBrains.Annotations;
+using Vostok.Configuration.Abstractions.SettingsTree;
+
+namespace Vostok.Configuration.Abstractions
+{
+    /// <summary>
+    /// Implements binding of <see cref="ISettingsNode"/>s to specific models.
+    /// </summary>
+    [PublicAPI]
+    public interface ISettingsBinder<out TSettings>
+    {
+        /// <summary>
+        /// <para>Binds the provided <see cref="ISettingsNode"/> instance to type <typeparamref name="TSettings"/>.</para>
+        /// <para>An exception will be thrown if the binding fails.</para>
+        /// </summary>
+        [NotNull]
+        TSettings Bind([NotNull] ISettingsNode rawSettings);
+    }
+}

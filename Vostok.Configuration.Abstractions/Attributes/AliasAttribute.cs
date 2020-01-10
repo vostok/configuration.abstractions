cb76e5f59274e17f608824341f845185e0fe6f45
levdimov <dimov@skbkontur.ru>
2020-01-10 14:14:18 +0400

    alias attribute

---
+++ Vostok.Configuration.Abstractions/Attributes/AliasAttribute.cs
@@ -0,0 +1,23 @@
+using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Abstractions.Attributes
+{
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property, AllowMultiple = true)]
+    public class AliasAttribute : Attribute
+    {
+        public AliasAttribute(string value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value));
+
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentException("Alias value cannot be empty or contains only whitespace characters", nameof(value));
+
+            Value = value;
+        }
+
+        public string Value { get; }
+    }
+}

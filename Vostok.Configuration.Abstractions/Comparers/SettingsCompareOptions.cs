dd7d3da1147ea7513caec4f789febdeda591490f
kungurtsev <kungurtsev@skbkontur.ru>
2020-01-22 15:26:47 +0500

    add SettingsNodeComparer

---
+++ Vostok.Configuration.Abstractions/Comparers/SettingsCompareOptions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Abstractions.Comparers
+{
+    [PublicAPI]
+    public class SettingsCompareOptions
+    {
+        public IReadOnlyCollection<IReadOnlyList<string>> ExcludedPaths;
+    }
+}

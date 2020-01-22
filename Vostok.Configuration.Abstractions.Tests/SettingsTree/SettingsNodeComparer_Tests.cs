dbd5f1f2bcf3cf1270eb6e3a63b6ac6b68f012b7
kungurtsev <kungurtsev@skbkontur.ru>
2020-01-22 19:04:18 +0500

    replace with FindDifferences

---
+++ Vostok.Configuration.Abstractions.Tests/SettingsTree/SettingsNodeComparer_Tests.cs
@@ -0,0 +1,191 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Vostok.Configuration.Abstractions.SettingsTree;
+
+namespace Vostok.Configuration.Abstractions.Tests.SettingsTree
+{
+    [TestFixture]
+    public class SettingsNodeComparer_Tests : TreeConstructionSet
+    {
+        [Test]
+        public void FindDifferences_should_be_empty_for_equal_nodes()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEmpty();
+        }
+
+        [Test]
+        public void FindDifferences_should_find_array_element()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "300"))));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEquivalentTo(
+                    new[]
+                    {
+                        new[] {"root", "array", "x"}
+                    });
+        }
+
+        [Test]
+        public void FindDifferences_should_find_property_value()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value3")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEquivalentTo(
+                    new[]
+                    {
+                        new[] {"root", "obj", "prop2"}
+                    });
+        }
+
+        [Test]
+        public void FindDifferences_should_find_missing_or_extra_properties()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop3", "value3")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEquivalentTo(
+                    new[] {"root", "obj", "prop2"},
+                    new[] {"root", "obj", "prop3"});
+        }
+
+        [Test]
+        public void FindDifferences_should_find_missing_or_extra_array_elements()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200")),
+                    Object("2", Value("x", "300"))));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEquivalentTo(
+                    new[] {"root", "array"},
+                    new[] {"root", "array", "x"});
+        }
+
+        [Test]
+        public void FindDifferences_should_find_missing_array_of_objects()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEquivalentTo(
+                    new[] {"root", "array"},
+                    new[] {"root", "array", "x"});
+        }
+
+        [Test]
+        public void FindDifferences_should_find_missing_object()
+        {
+            var left = Object(
+                "root",
+                Object("obj", ("prop1", "value1"), ("prop2", "value2")),
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            var right = Object(
+                "root",
+                Array(
+                    "array",
+                    Object("0", Value("x", "100")),
+                    Object("1", Value("x", "200"))));
+
+            SettingsNodeComparer.FindDifferences(left, right)
+                .Should()
+                .BeEquivalentTo(
+                    new[] {"root", "obj"},
+                    new[] {"root", "obj", "prop1"},
+                    new[] {"root", "obj", "prop2"});
+        }
+    }
+}
---
+++ Vostok.Configuration.Abstractions/SettingsTree/SettingsNodeComparer.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Vostok.Commons.Helpers.Comparers;
+using Path = System.Collections.Generic.List<string>;
+
+namespace Vostok.Configuration.Abstractions.SettingsTree
+{
+    [PublicAPI]
+    public class SettingsNodeComparer
+    {
+        public static IReadOnlyList<IReadOnlyList<string>> FindDifferences(
+            [CanBeNull] ISettingsNode left,
+            [CanBeNull] ISettingsNode right)
+        {
+            if (left?.Name != null || right?.Name != null)
+            {
+                left = new ObjectNode(null, new[] {left});
+                right = new ObjectNode(null, new[] {right});
+            }
+
+            var different = new HashSet<Path>(new ListComparer<string>());
+            Compare(new Path(), different, left, right);
+
+            return different.ToList();
+        }
+
+        private static void Compare(
+            [NotNull] Path path,
+            [NotNull] HashSet<Path> different,
+            [CanBeNull] ISettingsNode left,
+            [CanBeNull] ISettingsNode right)
+        {
+            if (ReferenceEquals(left, right))
+                return;
+
+            if (left?.Value != right?.Value || left == null != (right == null))
+                different.Add(path);
+
+            var leftDict = ChildrenToDictionary(left);
+            var rightDict = ChildrenToDictionary(right);
+
+            var allKeys = new HashSet<string>(leftDict.Keys.Concat(rightDict.Keys));
+
+            foreach (var key in allKeys)
+            {
+                leftDict.TryGetValue(key, out var l);
+                rightDict.TryGetValue(key, out var r);
+
+                var newPath = path;
+                var array = left is ArrayNode || right is ArrayNode;
+                if (!array)
+                    newPath = path.Concat(new[] {key}).ToList();
+
+                Compare(newPath, different, l, r);
+            }
+        }
+
+        private static Dictionary<string, ISettingsNode> ChildrenToDictionary([CanBeNull] ISettingsNode node)
+        {
+            if (node == null)
+                return new Dictionary<string, ISettingsNode>();
+
+            if (node is ArrayNode)
+                return node.Children.Select((x, i) => (x, i))
+                    .ToDictionary(
+                        x => x.i.ToString(),
+                        x => x.x);
+
+            return node.Children.ToDictionary(x => x.Name, x => x);
+        }
+    }
+}

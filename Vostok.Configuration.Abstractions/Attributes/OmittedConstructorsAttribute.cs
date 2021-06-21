145f73e397b4db474ed175713c6fc9e00022c683
Цуп Илья Олегович <tsup@skbkontur.ru>
2021-06-21 12:02:24 +0500

    Added OmmitedConstructorsAttribute

---
+++ Vostok.Configuration.Abstractions/Attributes/OmittedConstructorsAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Configuration.Abstractions.Attributes
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Specifies that an instance of this class may be created without parameterless constructors.
+    /// Note that classes created by this way will not have default member values specified in field/property initializers.
+    /// </summary>
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class OmittedConstructorsAttribute : Attribute
+    {
+    }
+}

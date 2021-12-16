13c7f37c0517e71c5494bfa9602945b71773407c
a.tolstov <a.tolstov@skbkontur.ru>
2021-12-16 14:16:27 +0500

    feat: NodeNameComparer

--- Vostok.Configuration.Abstractions/SettingsTree/Comparers.cs
+++ Vostok.Configuration.Abstractions/SettingsTree/Comparers.cs
@@ -6,5 +6,6 @@ namespace Vostok.Configuration.Abstractions.SettingsTree
     internal static class Comparers
     {
         public static readonly IEqualityComparer<string> NodeName = StringComparer.OrdinalIgnoreCase;
+        public static readonly IComparer<string> NodeNameComparer = StringComparer.OrdinalIgnoreCase;
     }
 }
